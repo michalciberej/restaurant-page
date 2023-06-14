09fa477afcd27ed434aa0b1b2055f04070bfef95
Michal Ciberej <michal.ciberej@gmail.com>
2023-06-14 15:48:24 +0200

    Add file,css and svg loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import "./style.css";
+import Svg from  "./img/p.svg"
+import imgOne from "./img/hamburger.jpg"
+import imgTwo from "./img/pizza.jpg"
+
 console.log("HELLO")
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,22 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     },
+    watch: true,
+    mode: "development",
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"]
+        },
+        {
+            test: /\.svg$/,
+            loader: 'svg-inline-loader'
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: 'file-loader'
+        },
+        ],
+    },
 };
