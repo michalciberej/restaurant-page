e1407d4140eac9d057a4c7719c8b38bf39addbaa
Michal Ciberej <michal.ciberej@gmail.com>
2023-06-14 20:08:07 +0200

    Add logo to home page

--- src/firstLoad.js
+++ src/firstLoad.js
@@ -1,44 +1,82 @@
 import pizza from "./img/pizza.jpg"
 
+export function createrNavBar() {
 
-export default function one() {
-const content = document.querySelector("#content")
+    const nav = document.createElement("div")
+    nav.setAttribute("id", "nav")
 
-const nav = document.createElement("div")
-nav.setAttribute("id", "nav")
+    const logo = document.createElement("div")
+    logo.textContent = "PIZZERIA"
+    logo.setAttribute("id", "logo")
+    nav.appendChild(logo)
 
-const logo = document.createElement("div")
-logo.textContent = "PizZA"
-logo.setAttribute("id", "logo")
-nav.appendChild(logo)
+    const ul = document.createElement("ul")
 
-const ul = document.createElement("ul")
+    const liOne = document.createElement("li")
+    liOne.textContent = "MENU"
+    liOne.classList.add("links")
+    liOne.setAttribute("id", "menuBtn")
+    ul.appendChild(liOne)
 
-const liOne = document.createElement("li")
-liOne.textContent = "MENU"
-liOne.classList.add("links")
-ul.appendChild(liOne)
+    const liTwo = document.createElement("li")
+    liTwo.textContent = "STORY"
+    liTwo.classList.add("links")
+    liOne.setAttribute("id", "storyBtn")
 
-const liTwo = document.createElement("li")
-liTwo.textContent = "STORY"
-liTwo.classList.add("links")
-ul.appendChild(liTwo)
+    ul.appendChild(liTwo)
 
-const liThree = document.createElement("li")
-liThree.textContent = "HOURS & LOCATION"
-liThree.classList.add("links")
-ul.appendChild(liThree)
+    const liThree = document.createElement("li")
+    liThree.textContent = "HOURS & LOCATION"
+    liThree.classList.add("links")
+    liOne.setAttribute("id", "halBtn")
 
-nav.appendChild(ul)
+    ul.appendChild(liThree)
+    nav.appendChild(ul)
+    document.querySelector("#content").appendChild(nav)
+}
 
-content.appendChild(nav)
+export function createImgBody() {
 
+    const imgContainer = document.createElement("div")
+    imgContainer.setAttribute("id", "imgContainer")
+    imgContainer.style.background = `url(${pizza})`
+    imgContainer.style.backgroundSize = "cover"
+    imgContainer.style.backgroundRepeat = "no-repeat"
+    document.querySelector("#content").appendChild(imgContainer)
+}
 
+export function createHomeLogo() {
 
-const imgContainer = document.createElement("div")
-imgContainer.setAttribute("id", "imgContainer")
-imgContainer.style.background = `url(${pizza})`
-imgContainer.style.backgroundSize = "cover"
-imgContainer.style.backgroundRepeat = "no-repeat"
-content.appendChild(imgContainer)
+    const logoContainer = document.createElement("div")
+    logoContainer.setAttribute("id", "homeLogoContainer")
+
+    const secondaryText = document.createElement("p")
+    secondaryText.textContent = "FRESH HANDMADE PIZZA"
+    secondaryText.setAttribute("id", "secondaryLogoText")
+    logoContainer.appendChild(secondaryText)
+
+    const mainText = document.createElement("h1")
+    mainText.textContent = "PIZZERIA"
+    mainText.setAttribute("id", "mainLogoText")
+    logoContainer.appendChild(mainText)
+
+    document.querySelector("#content").appendChild(logoContainer)
+}
+
+export function createLogo() {
+
+    const logoContainer = document.createElement("div")
+    logoContainer.setAttribute("id", "LogoContainer")
+
+    const secondaryText = document.createElement("p")
+    secondaryText.textContent = "FRESH HANDMADE PIZZA"
+    secondaryText.setAttribute("id", "secondaryLogoText")
+    logoContainer.appendChild(secondaryText)
+
+    const mainText = document.createElement("h1")
+    mainText.textContent = "PIZZERIA"
+    mainText.setAttribute("id", "mainLogoText")
+    logoContainer.appendChild(mainText)
+
+    document.querySelector("#content").appendChild(logoContainer)
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./style.css";
-import one from "./firstLoad"
 import Svg from  "./img/p.svg"
 import burger from "./img/hamburger.jpg"
-import pizza from "./img/pizza.jpg"
+import { createrNavBar, createImgBody, createHomeLogo } from "./firstLoad";
 
-one()
+const menuBtn = document.querySelector("#menuBtn");
+
+createrNavBar()
+createImgBody()
+createHomeLogo()
+
+//menuBtn.addEventListener("click". () => displayMenuPage())
